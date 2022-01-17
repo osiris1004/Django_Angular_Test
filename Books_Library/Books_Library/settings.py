436ca0a6a5932dc984baea8f599158a29af85186
osiris <kontchou05@gmail.com>
2022-01-17 09:32:06 +0100

    v2

--- Books_Library/Books_Library/settings.py
+++ Books_Library/Books_Library/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'EmployeeApp.apps.EmployeeappConfig',
+    'rest_framework',
 ]
 
-CORS_ORIGIN_ALLOW_ALL ="True"
+CORS_ORIGIN_ALLOW_ALL =True
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleWare',
--- Books_Library/EmployeeApp/apps.py
+++ Books_Library/EmployeeApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EmployeeConfig(AppConfig):
+class EmployeeappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Employee'
+    name = 'EmployeeApp'
